72b842a8b7c2b3447d470c5019cd6f7b5e36b270
pgambino-espear <pgambino@espear.com>
2020-02-13 18:27:23 -0500

    Added welcome message.

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const server = require("./server.js");
 
 const port = process.env.PORT || 5000;
 
+
+server.get("/", (req, res) => {
+  
+  res.status(200).json({message: "Hello Heroku!");
+})
+
 server.listen(port, () => {
   console.log(`\n* Server Running on ${port} *\n`);
 });
